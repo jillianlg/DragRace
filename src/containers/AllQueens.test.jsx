ae653665105effc6fc23256f1b9006bc8a5c2f2d
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-08 13:56:08 -0800

    adds AllQueens test - passes as expected

---
+++ src/containers/AllQueens.test.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+
+describe('AllQueens container', () => {
+  it('displays a loading screen then a list of queens', () => {
+
+  });
+});
