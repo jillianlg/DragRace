eb9151ca99143df74c2803a49a2f0c8e8b0db6d9
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-08 15:09:57 -0800

    imports Queen component to QueenList component, passes down props, adds testid and returns queenElements

--- src/components/app/Queens/QueenList.jsx
+++ src/components/app/Queens/QueenList.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Queen from './Queen';
 
-const QueenList = () => ();
+const QueenList = ({ queens }) => {
+  const queenElements = queens.map((queen) => (
+    <li key={queen.name}>
+      <Queen
+        image_url={queen.image_url}
+        name={queen.name}
+      />
+    </li>
+  ));
+
+  return <ul data-testid="queens">{queenElements}</ul>;
+};
 
 QueenList.propTypes = {
-  queens: propTypes.arrayOf(PropTypes.shape({
+  queens: PropTypes.arrayOf(PropTypes.shape({
     image_url: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired
   })).isRequired
--- src/containers/AllQueens.jsx
+++ src/containers/AllQueens.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import QueenList from '../components/app/Queens/QueenList';
 
 const AllQueens = () => {
   const [loading, setLoading] = useState(true);
