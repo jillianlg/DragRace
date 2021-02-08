bac43f78fafc696c8ab64ab7a86e4ab4d3ec4d65
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-08 14:10:46 -0800

    adds and tests loading to AllQueens contanier - test passes as expected

--- src/containers/AllQueens.jsx
+++ src/containers/AllQueens.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AllQueens = () => {
+  const [loading, setLoading] = useState(true);
+  const [queens, setQueens] = useState([]);
 
+  if(loading) return <h1>LOADING</h1>;
+
+  return <QueenList queens={queens} />;
 };
-
 
 export default AllQueens;
--- src/containers/AllQueens.test.jsx
+++ src/containers/AllQueens.test.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
+import AllQueens from './AllQueens';
 
 describe('AllQueens container', () => {
   it('displays a loading screen then a list of queens', () => {
+    render(<AllQueens />);
 
+    screen.getByText('LOADING');
   });
 });
