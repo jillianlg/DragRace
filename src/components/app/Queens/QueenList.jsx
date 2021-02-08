73facefbbdcb3282d399d0c061fe8a9b6dc5a0c5
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-08 14:59:52 -0800

    starts QueenList component and passes down props

--- src/components/app/Queens/QueenList.jsx
+++ src/components/app/Queens/QueenList.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 
+const QueenList = () => ();
+
+QueenList.propTypes = {
+  queens: propTypes.arrayOf(PropTypes.shape({
+    image_url: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
+  })).isRequired
+};
+
+export default QueenList;
--- src/containers/AllQueens.test.jsx
+++ src/containers/AllQueens.test.jsx
@@ -3,9 +3,11 @@ import { render, screen, waitFor } from '@testing-library/react';
 import AllQueens from './AllQueens';
 
 describe('AllQueens container', () => {
-  it('displays a loading screen then a list of queens', () => {
+  it('displays a loading screen then a list of queens', async() => {
     render(<AllQueens />);
 
     screen.getByText('LOADING');
+
+    const ul = await screen.findByTestId('queens');
   });
 });
