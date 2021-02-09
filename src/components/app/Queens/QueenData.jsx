b0db3a87e3f3f5dc1ef5ff4e1c4d863e3ed7e07a
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-08 18:56:42 -0800

    adds basic css to QueenData

--- src/components/app/Queens/QueenData.jsx
+++ src/components/app/Queens/QueenData.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './QueenData.css';
 
 const QueenData = ({ image_url, name, seasons:[season], quote }) => (
   <div data-testid="details">
-    <p>{name}</p>
-    <img style={{ height: '200px' }} src={image_url} alt={name} />
-    <p>{season.id}</p>
-    <p>{quote}</p>
+    <section className={styles.QueenData}>
+      <p>{name}</p>
+      <img style={{ height: '200px' }} src={image_url} alt={name} />
+      <p>Season:{season.id}</p>
+      <p>{quote}</p>
+    </section>
   </div>
 );
 
