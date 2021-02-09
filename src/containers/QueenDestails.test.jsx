5b1f8d51cc4d9d8bf71fcb3ba588b85f32be8fe3
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-08 16:14:01 -0800

    adds QueenDetails loading and finished test - test fails as expected

--- src/containers/QueenDestails.test.jsx
+++ src/containers/QueenDestails.test.jsx
@@ -1,2 +1,16 @@
 import { render, screen, waitFor } from '@testing-library/react';
+import QueenDetails from './QueenDetails';
 
+describe('AllQueens container', () => {
+  it('displays a loading screen then a list of queens', async() => {
+    render(<QueenDetails />);
+
+    screen.getByText('LOADING');
+
+    const ul = await screen.findByTestId('details');
+
+    return waitFor(() => {
+      expect(ul).not.toBeEmptyDOMElement();
+    });
+  });
+});
--- src/containers/QueenDetails.jsx
+++ src/containers/QueenDetails.jsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import QueenData from '../components/app/Queens/QueenData';
 
 const QueenDetails = () => {
   const [loading, setLoading] = useState(true);
   const [details, setDetails] = useState([]);
-}
+
+  if(loading) return <h1>LOADING</h1>;
+  return <QueenData details={details} />;
+};
+
+export default QueenDetails;
