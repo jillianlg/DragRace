753deae3be5dee80e39e8b3606c18a47d779d684
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-08 18:10:53 -0800

    adds QueenData snapshot test - test passes as expected

--- src/components/app/Queens/QueenData.test.jsx
+++ src/components/app/Queens/QueenData.test.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-import Queen from './Queen';
+import QueenData from './QueenData';
 
 
-describe('Queen component', () => {
+
+describe('QueenData component', () => {
   afterEach(() => cleanup());
-  it('renders Queen', () => {
+  it('renders Queen data by id', () => {
     const { asFragment } = render(
-      <Queen
+      <QueenData
         name="Bianca Del Rio"
         image_url="BDR.com"
+        seasons="6"
+        quote="Not today Satan."
       />);
     expect(asFragment()).toMatchSnapshot();
   });
